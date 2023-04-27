37390ad0f3884d7b6cb7838509915869c5423c61
Satpute <rutuj.satpute@dxc.com>
2023-04-27 10:17:26 +0530

    update

---
+++ Library/ClassLibrary1/Class1.cs
@@ -0,0 +1,26 @@
+﻿namespace ClassLibrary1
+{
+  
+    public class Calculator
+    {
+        public int Sum(int a, int b)
+        {
+            return a + b;
+        }
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+        public int Divide(int a, int b)
+        {
+            return (a / b);
+        }
+
+
+
+    }
+}
---
+++ Library/ConsoleApp1/Program.cs
@@ -0,0 +1,50 @@
+﻿using ClassLibrary1;
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calc = new Calculator();
+            Console.WriteLine("Enter the first Number: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Second Number: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("What Operation would you like to do:");
+            Console.WriteLine("1 - Addition \n2 - Subtraction\n3 - Multiplication\n4 - Division");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    {
+                        int c = calc.Sum(a, b);
+                        Console.WriteLine($"Addtion of {a} and {b} is {c}");
+                        break;
+                    }
+                case 2:
+                    {
+                        int c = calc.Subtract(a, b);
+                        Console.WriteLine($"Subtraction of {a} and {b} is {c}");
+                        break;
+                    }
+                case 3:
+                    {
+                        int c = calc.Multiply(a, b);
+                        Console.WriteLine($"Multiplication of {a} and {b} is {c}");
+                        break;
+                    }
+                case 4:
+                    {
+                        int c = calc.Divide(a, b);
+                        Console.WriteLine($"Division of {a} and {b} is {c}");
+                        break;
+                    }
+            }
+   
+        Console.ReadKey();
+
+        }
+       
+    }
+}
